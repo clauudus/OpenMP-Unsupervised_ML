676a007c76fd40e3da553ae6701c56311904a6e4
clauudus <96309298+clauudus@users.noreply.github.com>
2026-01-27 11:24:20 +0100

    Create exec.sh
    Same execution script as the Floyd-Warshall one. The only change is the name of the binaries.
    Don't forget to chmod this as well! :)

---
+++ K-means/exec.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+variants=("K-means_OpenMP" "K-means_scalar")
+
+files=()
+
+for opt in 2 3; do
+	for native in 0 1; do
+		march=""
+		if [ $native = 1 ]; then
+			march="-march=native"
+		fi
+		make clean
+		make OPTFLAGS="-O${opt} ${march}"
+
+		for v in "${variants[@]}; do
+			if [-x "./$v" ]; then
+				new="./${v}-${opt}-${native}"
+				cp "./$v" "$new"
+				chmod +x "$new"
+				files+=("$new")
+			fi
+		done
+	done
+done
+
+hyperfine $files
